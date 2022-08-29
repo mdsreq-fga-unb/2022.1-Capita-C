f4fc0ec6c07dbc66d426fa1aa17ef23c5c59a787
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2022-08-28 22:06:09 -0300

    Finalização do login

--- backend/src/routes/api/userRoute.ts
+++ backend/src/routes/api/userRoute.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import userController from "../../controllers/userController";
+import { isAdmin, isConsultor } from "../../middlewares/authMiddlewares";
 
 const router = Router();
 
 router.get("/", userController.list);
-router.get("/:cpf", userController.retrieve);
-router.patch("/:cpf", userController.update);
-router.delete("/:cpf", userController.destroy);
+router.get("/search/:cpf", userController.retrieve);
+router.patch("/search/:cpf", userController.update);
+router.delete("/search/:cpf", userController.destroy);
 
+router.get("/test", isConsultor, (req, res) => res.json(req.user));
 export default router;
--- backend/src/routes/apiRoute.ts
+++ backend/src/routes/apiRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRoute from "./api/userRoute";
+import authRoute from "./api/authRoute";
+import { isAuthenticated } from "../middlewares/authMiddlewares";
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("Bem vindo ao CapitaC API"));
-router.use("/user", userRoute);
-
+router.use("/user", isAuthenticated, userRoute);
+router.use("/auth", authRoute);
 export default router;
