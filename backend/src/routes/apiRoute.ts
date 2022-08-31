0e24c2be79ae4d69b36eb75a0135d062198df106
augustocrmg <augusto.duraes33@gmail.com>
2022-08-31 02:47:16 -0300

    Controllers de email

--- backend/src/routes/apiRoute.ts
+++ backend/src/routes/apiRoute.ts
@@ -3,6 +3,7 @@ import userRoute from "./api/userRoute";
 import authRoute from "./api/authRoute";
 import cadastroCnpjRoute from "./api/cadastroCnpjRoute";
 import telefoneRoute from "./api/telefoneRoute";
+import emailRoute from "./api/emailRoute";
 import { isAuthenticated } from "../middlewares/authMiddlewares";
 
 const router = Router();
@@ -11,6 +12,7 @@ router.get("/", (req, res) => res.send("Bem vindo ao CapitaC API"));
 router.use("/user", isAuthenticated, userRoute);
 router.use("/cnpj", isAuthenticated, cadastroCnpjRoute);
 router.use("/telefone", isAuthenticated, telefoneRoute);
+router.use("/email", isAuthenticated, emailRoute);
 router.use("/auth", authRoute);
 
 export default router;
