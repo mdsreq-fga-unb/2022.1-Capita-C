b874b7704f2baba741d8548b88d9c3df9d4ddb09
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2022-09-02 03:38:19 -0300

    atualizando schema - chave renomeada

--- backend/prisma/migrations/20220902000541_new/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "CadastroCnpj" DROP CONSTRAINT "CadastroCnpj_responsavel_fkey";
-
--- AddForeignKey
-ALTER TABLE "CadastroCnpj" ADD CONSTRAINT "CadastroCnpj_responsavel_fkey" FOREIGN KEY ("responsavel") REFERENCES "User"("cpf") ON DELETE SET NULL ON UPDATE CASCADE;
