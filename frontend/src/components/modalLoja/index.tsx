56b269de1be179a24268f4aab93edd1d4a0a8387
alexianaa <alexiasilvacardosoo@gmail.com>
2022-09-07 18:09:26 -0300

    finaliza estilizacao do modal das lojas

--- frontend/src/components/modalLoja/index.tsx
+++ frontend/src/components/modalLoja/index.tsx
@@ -40,14 +40,76 @@ const ModalLoja = ({ closeModal }: any, lojaInfo: any) => {
     return (
         <div className="modalDiv" >
             <div className="boxContent">
-            <button onClick={() => closeModal(false)} >Cancelar</button>
-                <text className="Title" >Loja</text>
-                <text className="inputTitle" >Nome da loja</text>
-                <input className="inputBox" name="name" value={loja?.nomeFantasia} placeholder={lojaInfo?.nomeFantasia} /* onChange={} */ ></input>
-                <text className="inputTitle" >CNPJ</text>
-                <input className="inputBox" name="cnpj" value={loja?.cnpjFinal} placeholder={lojaInfo?.cnpjFinal} ></input>
-                <text className="SectionTitle">Contato</text>
-                <text className="SectionTitle">Endereço</text>
+                <div className="section" >
+                    <text className="title" >Loja</text>
+
+                    <text className="inputTitle" >Nome da loja</text>
+                    <input className="inputBox" name="name" value={loja?.nomeFantasia} placeholder={lojaInfo?.nomeFantasia} /* onChange={} */ ></input>
+                    <text className="inputTitle" >CNPJ</text>
+                    <input className="inputBox" name="cnpj" value={loja?.cnpjFinal} placeholder={lojaInfo?.status} ></input>
+                    <text className="inputTitle" >Identificador</text>
+                    <select className="inputBox" /* { loja?.identificadorMatrizFiliar } */>
+                        <option value="Matriz" >Matriz</option> {/**/}
+                        <option value="Filial" >Filial</option>
+                    </select>
+                    <div className="minorDiv">
+                        <text className="inputTitle" >Atribuição</text>
+                        <text className="inputTitle" >Status de Parceria</text>
+                    </div>
+                    <div className="minorDiv" >
+                        <select className="minorInput" /* { loja?.atribuido? 'Verdadeiro': 'Falso' } */>
+                            <option value="atribuidoVer" >Verdadeiro</option>
+                            <option value="atribuidoFal" >Falso</option>
+                        </select>
+                        <select className="minorInput" /* { loja?.parceriaAceita */>
+                            <option value="parceriaSuc" >Secesso</option>
+                            <option value="parceriaPro" >Processando</option>
+                            <option value="parceriaRec" >Recusado</option>
+                        </select>
+                    </div>
+                    <text className="inputTitle" >Cnaes</text>
+                    <input className="inputBox" name="cnaes" value={loja?.cnaes} placeholder={lojaInfo?.cnaes} ></input>
+                </div>
+                <div className="section" >
+                    <text style={{ marginTop: 5, marginBottom: 5 }} >Contato</text>
+
+                    <text className="inputTitle" >Telefone</text>
+                    <input className="inputBox" name="telefone" /* value={loja?.telefone[0]} placeholder={lojaInfo?.telefone[0]} */ ></input>
+                    <text className="inputTitle" >Telefone Secundário</text>
+                    <input className="inputBox" name="telefoneSec" /* value={loja?.telefone[1]} placeholder={lojaInfo?.telefone[1]} */ ></input>
+                    <text className="inputTitle" >E-mail</text>
+                    <input className="inputBox" name="email" /* value={loja?.correioEletronico[0]} placeholder={lojaInfo?.correioEletronico[0]} */ ></input>
+                </div>
+                <div className="section" >
+                    <text style={{ marginTop: 5, marginBottom: 5 }} >Endereço</text>
+
+                    <text className="inputTitle" >CEP</text>
+                    <input className="inputBox" name="cep" /* value={loja?.cep} placeholder={lojaInfo?.cep} */ ></input>
+                    <text className="inputTitle" >Logradouro</text>
+                    <input className="inputBox" name="logradouro" /* value={loja?.logadouro} placeholder={lojaInfo?.logadouro} */ ></input>
+                    <div className="minorDiv">
+                        <text className="inputTitle" >Bairro</text>
+                        <text className="inputTitle" >Estado</text>
+                    </div>
+                    <div className="minorDiv" >
+                        <input className="minorInput" name="complemento" /* value={loja?.bairro} placeholder={lojaInfo?.bairro} */ ></input>
+                        <input className="minorInput" name="estado" /* value={loja?.unidadeFederativa} placeholder={lojaInfo?.unidadeFederativa} */ ></input>
+                    </div>
+                    <text className="inputTitle" >Observação</text>
+                    <input className="inputBox" name="observacao" style={{ height: 50 }} /* value={loja?.complemento} placeholder={lojaInfo?.complemento} */ ></input>
+                </div>
+                <div className="buttons">
+                    <span className="botao" >
+                        <text className="botaoText">
+                            Atualizar
+                        </text>
+                    </span>
+                    <span className="botao" onClick={() => closeModal(false)} >
+                        <text className="botaoText">
+                            Cancelar
+                        </text>
+                    </span>
+                </div>
             </div>
         </div>
     )
