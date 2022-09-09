9aafb31f0081131a908dc688dae229f5f5a998d8
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2022-09-09 00:44:45 -0300

    Mapping da Barra Lateral
    Criado um mapeador para a barra lateral, onde ao clicar redireciona para a página desejada.

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -3,8 +3,27 @@ import OptionSidebar from '../../images/option-sidebar.svg'
 import MenuHamburguer from '../../images/menu-hamburguer.svg'
 import './index.css'
 import { Link } from 'react-router-dom'
+import {indexData} from './indexData'
 
-export function Sidebar() {
+export function Sidebar(){
+  return(
+    <div className="sidebar">
+        <ul>
+            {indexData.map((val,key)=>{
+                return <li key={key} onClick={()=>{window.location.pathname = val.link}}> 
+                {" "}
+                    <div>{val.icon}</div>{" "}
+                    <div>
+                        {val.title}
+                    </div>
+                </li>
+            })}
+        </ul>
+    </div>
+  )
+}
+
+{/*export function Sidebar() {
 
     return (
         <div className='sidebar'>
@@ -12,18 +31,18 @@ export function Sidebar() {
                 <ul>
                     <div className='menu'>   
                         <img className='menu-hamburguer' src={MenuHamburguer}/>        
-                        <Link to="/lojas">Lojas</Link>
-                        {/*<span className='lojas'>Lojas</span>*/}
+                        <span className='loja'>Loja</span>
+                        {<Link to="/lojas">Lojas</Link>}
                     </div>
                     <div className='menu2'>   
                         <img className='menu-hamburguer' src={MenuHamburguer}/>
                         <Link to="/perfil">Perfil</Link>        
-                        {/*<span className='perfil'>Perfil</span>*/}
+                        {<span className='perfil'>Perfil</span>}
                     </div>
                     <div className='menu3'>   
                         <img className='menu-hamburguer' src={MenuHamburguer}/>
                         <Link to="/usuarios">Usuarios</Link>        
-                        {/*<span className='Usuarios'>Usuarios</span>*/}
+                        {<span className='Usuarios'>Usuarios</span>}
                     </div>
                 </ul>
             </div>
@@ -31,4 +50,4 @@ export function Sidebar() {
         </div>
         
     );
-}
+}*/}
---
+++ frontend/src/components/sidebar/indexData.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MenuHamburguer from '../../images/menu-hamburguer.svg'
+
+export const indexData = [
+  {
+    title: "Lojas",
+    icon: <img className='menu-hamburguer' src={MenuHamburguer}/>,
+    link: "/home"
+  },
+  {
+    title: "Perfil",
+    icon: <img className='menu-hamburguer' src={MenuHamburguer}/>,
+    link: "/home"
+  },
+  {
+    title: "Usuario",
+    icon: <img className='menu-hamburguer' src={MenuHamburguer}/>,
+    link: "/home"
+  }
+]
