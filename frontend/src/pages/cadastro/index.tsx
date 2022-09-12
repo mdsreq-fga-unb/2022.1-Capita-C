7cddf81ea8f9d45f5a3eaa56495a58f9342ce2e6
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2022-09-12 03:19:14 -0300

    Esqueleto feito
    Feito os esqueletos para as funcionalidades, implementação como um todo no próximo commit.

--- frontend/src/pages/cadastro/index.tsx
+++ frontend/src/pages/cadastro/index.tsx
@@ -1,1 +1,34 @@
-import React from "react";
+import userEvent from "@testing-library/user-event";
+import React, { ChangeEvent, useContext, useState } from "react";
+import { Header } from '../../components/header';
+import { Sidebar } from '../../components/sidebar';
+import './index.css';
+
+function Cadastro(){
+  return (
+
+    <div className="main">
+     <Header />
+     <div className="sidebar-index">
+      <Sidebar />
+      <div className="index">
+        <div className="addUsuario">Adicionar usuario</div>
+        <div className="adicionaUsuario">
+          <text className="ImputText">Nome do usuario</text>
+          <input className="Input" name="nome" value={user.name}/>
+          <text className="ImputText">CPF</text>
+          <input className="Input" name="cpf" value={user.cpf}/>
+          <text className="ImputText">Telefone</text>
+          <input className="Input" name="telefone" value={telefone.numeroTelefone}/>
+          <text className="ImputText">E-mail</text>
+          <input className="Input" name="email" value={email.email}/>
+          <text className="ImputText">Senha</text>
+          <input className="Input" name="senha" value={user.password}/>
+          <button id="btn" onClick={() => {handleClick()}}>CRIAR</button>
+        </div>
+      </div>
+     </div>
+    </div>
+
+)
+}
--- frontend/src/pages/usuarioAdmin/index.tsx
+++ frontend/src/pages/usuarioAdmin/index.tsx
@@ -1,1 +1,39 @@
-import React from "react";
+import React, { ChangeEvent, useContext, useState } from "react";
+import { Header } from '../../components/header';
+import { Sidebar } from '../../components/sidebar';
+import './index.css';
+import editIcon from '../../images/edit-icon.svg'
+import deleteIcon from '../../images/delete-icon.svg'
+
+function Usuario(){
+
+  return(
+  
+    <div className="Usuarios">
+      <Header />
+      <div className="sidebar-index">
+        <Sidebar />
+        <div className="index">
+        <div className="header-usuarios">
+                        <span id='totalUsuarios' >Total de usuarios: {count}</span>
+                        <span id='adicionarUsuario'>
+                            <div id='addUsuariosText'>
+                                + Adicionar Usuario
+                            </div>
+                        </span>
+                    </div>
+                    <text id='titulo' > USUARIOS</text>
+                    <div className="cards">
+                        {Usuario.map((element, i) => {
+                            return (
+                                <CardUsuario nome={element.nome_fantasia} cnpj={element.cnpj} telefone={element.ddd_telefone_1} cep={element.cep} bairro={element.bairro} />
+                            )
+                        })}
+                    </div>
+        </div>
+      </div>
+
+    </div>
+    
+  )
+}
